ff3d7ddaa295a66ea4b907f88c8d90d8dfff03bb
Leo Lebtahi <lebtahileo@gmail.com>
2026-05-06 16:47:15 +0200

    Modified uppercase.py so that it include format ()

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 
 def uppercase(str):
-    result = ""
-
     for c in str:
-        if ord('a') <= ord(c) <= ord('z'):
-            result += chr(ord(c) - 32)
+        if ord(c) >= 97 and ord(c) <= 122:
+            print("{}".format(chr(ord(c) - 32)), end="")
         else:
-            result += c
-
-    print(result)
+            print("{}".format(c), end="")
+    print()
