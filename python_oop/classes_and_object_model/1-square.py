2f3a04b7c2cdc33c0a93412a37ba35ddfebcfa00
Leo Lebtahi <lebtahileo@gmail.com>
2026-05-12 16:33:51 +0200

    Define a class square with a private instance attribute size

---
+++ python_oop/classes_and_object_model/1-square.py
@@ -0,0 +1,13 @@
+class Square:
+    """
+    Defines a square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes the square with a private size attribute.
+
+        Args:
+            size: size of the square
+        """
+        self.__size = size
