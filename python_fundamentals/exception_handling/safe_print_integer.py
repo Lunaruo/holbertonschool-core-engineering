1ce1fa8e91596da8dda9bd981c6ca91663905fa2
Leo Lebtahi <lebtahileo@gmail.com>
2026-05-13 15:50:54 +0200

    Added shebang and comments

--- python_fundamentals/exception_handling/safe_print_integer.py
+++ python_fundamentals/exception_handling/safe_print_integer.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python3
+
 def safe_print_integer(value):
+"""print an integer with {:d} format"""
     try:
         print("{:d}".format(value))
         return True
     except (ValueError, TypeError):
         return False
+
