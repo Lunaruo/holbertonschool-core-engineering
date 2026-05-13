722bd2f97acd4b785b52bd055ef10d249895777f
Leo Lebtahi <lebtahileo@gmail.com>
2026-05-13 16:04:23 +0200

    Added an additional comment

--- python_fundamentals/exception_handling/safe_print_integer.py
+++ python_fundamentals/exception_handling/safe_print_integer.py
@@ -2,6 +2,7 @@
 
 def safe_print_integer(value):
     """print an integer with {:d} format"""
+    """return false if value not an integer"""
     try:
         print("{:d}".format(value))
         return True
