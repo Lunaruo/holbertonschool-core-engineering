5a1395398743935d3a96b28b4dae12034db3b1e8
Leo Lebtahi <lebtahileo@gmail.com>
2026-05-15 10:33:22 +0200

    Add validations to the size attribute on the Square class

---
+++ python_oop/classes_and_object_model/2-square.py
@@ -0,0 +1,20 @@
+class Square:
+    """
+    Defines a square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes the square with validation.
+
+        Args:
+            size (int): size of the square
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
