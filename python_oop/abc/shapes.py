1136c98dacfa69c6fb9df5238c775b4fbc2ef615
Leo Lebtahi <lebtahileo@gmail.com>
2026-05-17 23:03:23 +0200

    Create an abstract class named Shape with two abstract methods: area and perimeter.

---
+++ python_oop/abc/shapes.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+"""
+Defines abstract Shape class and shape subclasses.
+"""
+
+from abc import ABC, abstractmethod
+import math
+
+
+class Shape(ABC):
+    """
+    Abstract base class for shapes.
+    """
+
+    @abstractmethod
+    def area(self):
+        """
+        Returns the area of the shape.
+        """
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        """
+        Returns the perimeter of the shape.
+        """
+        pass
+
+
+class Circle(Shape):
+    """
+    Represents a circle.
+    """
+
+    def __init__(self, radius):
+        """
+        Initializes the circle.
+
+        Args:
+            radius (float): radius of the circle
+        """
+
+        self.radius = radius
+
+    def area(self):
+        """
+        Returns the area of the circle.
+        """
+
+        return math.pi * (self.radius ** 2)
+
+    def perimeter(self):
+        """
+        Returns the perimeter of the circle.
+        """
+
+        return 2 * math.pi * self.radius
+
+
+class Rectangle(Shape):
+    """
+    Represents a rectangle.
+    """
+
+    def __init__(self, width, height):
+        """
+        Initializes the rectangle.
+
+        Args:
+            width (int): width of the rectangle
+            height (int): height of the rectangle
+        """
+
+        self.width = width
+        self.height = height
+
+    def area(self):
+        """
+        Returns the area of the rectangle.
+        """
+
+        return self.width * self.height
+
+    def perimeter(self):
+        """
+        Returns the perimeter of the rectangle.
+        """
+
+        return 2 * (self.width + self.height)
+
+
+def shape_info(shape):
+    """
+    Prints area and perimeter of a shape.
+
+    Args:
+        shape: object with area and perimeter methods
+    """
+
+    print("Area: {}".format(shape.area()))
+    print("Perimeter: {}".format(shape.perimeter()))
